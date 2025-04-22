6323c4cc7f7701cd9baed5e63ba9255100a82e04
ernest.prykhodko <ernest.prykhodko@kitrum.com>
2025-04-22 23:02:45 +0300

    basic auth

---
+++ client/src/views/account.tsx
@@ -0,0 +1,10 @@
+const Account = () => {
+  return (
+    <div>
+      <h1>Account</h1>
+      <p>This is the account page.</p>
+    </div>
+  )
+}
+
+export default Account
---
+++ client/src/views/signin.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+const Signin = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const login = async () => {
+    const res = await axios.post("http://127.0.0.1:8000/auth/login", {
+        email: username,
+        password: password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        withCredentials: true,
+      }  
+    )
+    console.log(res.data)
+    if (res.data.access_token) {
+      localStorage.setItem("token", res.data.access_token)
+      window.open("/account", "_self")
+    } else {
+      alert("Invalid credentials")
+    }
+  }
+
+  return (
+    <div>
+      <fieldset className="fieldset">
+        <legend className="fieldset-legend">Signin</legend>
+        <input type="text" className="input" placeholder="Username" 
+          onChange={(e) => setUsername(e.target.value)} 
+          value={username}
+        />
+        <br />
+        <br />
+        <input type="password" className="input" placeholder="Password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password}  
+        />
+        <br />
+        <br />
+        <button className="btn" onClick={login}>Signin</button>
+      </fieldset>
+
+      <br />
+
+      <fieldset className="fieldset">
+        <legend className="fieldset-legend">Signup</legend>
+        <input type="text" className="input" placeholder="Username" />
+        <br />
+        <br />
+        <input type="password" className="input" placeholder="Password" />
+        <br />
+        <br />
+        <button className="btn">Signup</button>
+      </fieldset>
+    </div>
+  )
+}
+
+export default Signin
